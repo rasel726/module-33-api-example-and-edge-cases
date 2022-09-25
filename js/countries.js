0e77c722d11a75dfc44b6658d0752fce7a9d2e3d
rasel726 <md.rasel2726@gmail.com>
2022-09-26 00:20:00 +0600

    33-4 (advanced) Call dynamic API, load dynamic data to display

--- js/countries.js
+++ js/countries.js
@@ -12,12 +12,32 @@ const displayCountries = countries => {
     countries.forEach(country => {
         const countryDiv = document.createElement('div');
         countryDiv.classList.add('country');
+        // console.log(country);
         countryDiv.innerHTML = `
             <h3>Name: ${country.name.common}</h3>
             <p>Capital: ${country.capital ? country.capital[0] : 'No Capital'}</p>
+            <button onclick="loadCountryDetails('${country.cca2}')">Details</button>
         `;
         countriesContainer.appendChild(countryDiv);
     });
+}
+
+const loadCountryDetails = (code) => {
+    // https://restcountries.com/v3.1/alpha/{code}
+    const url = `https://restcountries.com/v3.1/alpha/${code}`
+    // console.log('get country details', code);
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayCountryDetail(data[0]))
+}
+
+const displayCountryDetail = country => {
+    console.log(country);
+    const countryDetail = document.getElementById('country-detail');
+    countryDetail.innerHTML = `
+        <h2>Details: ${country.name.common}</h2>
+        <img src="${country.flags.png}">
+    `
 }
 
 loadCountries();
